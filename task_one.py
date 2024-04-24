421cc59dd5cf750b3c22071364b0ddc7864b5232
Surveyor Jr <matingonk@gmail.com>
2024-04-24 17:35:09 +0200

    Create task_one.py

---
+++ task_one.py
@@ -0,0 +1,18 @@
+def plot_data(my_file):
+    '''
+    This function plots a scatterplot provided with a coordinate text file path 
+
+    Parameters
+    ----------
+    my_file : STRING 
+        Required path to the file with coordinates in text format 
+
+    Returns
+    ---------
+    Scatterplot 
+    '''
+    scatter_plot = None 
+    """
+    ==> Write your code here <==
+    """
+    return scatter_plot 
